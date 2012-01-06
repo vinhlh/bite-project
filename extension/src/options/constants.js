4be793caea22492378f02f85ee66bb9afa868a3d
Jason Stredwick <jason.stredwick@gmail.com>
2012-01-05 21:07:02 -0800

    Set the current version and changed some default values for the server and bug console visibility.

--- extension/src/options/constants.js
+++ extension/src/options/constants.js
@@ -109,7 +109,7 @@ bite.options.constants.ThreeWayOption = {
  * @enum {string}
  */
 bite.options.constants.ServerChannelOption = {
-  DEV: 'https://dev-dot-bite-playground.appspot.com',
+  DEV: 'https://bite-playground-dev.appspot.com',
   RELEASE: 'https://bite-playground.appspot.com'
 };
 
