98a231719f2f323e90ca721c0de7b424b2265a9e
Jason Stredwick <jason.stredwick@gmail.com>
2013-02-10 15:08:59 -0800

    Update rpf and google analytics for version 2 of chrome extensions.

--- common/extension/analytics/analytics.js
+++ common/extension/analytics/analytics.js
@@ -2,8 +2,16 @@ var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-20998404-1']);
 _gaq.push(['_trackPageview']);
 
-(function() {
+/*(function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();*/
+
+
+// Required to always be secure for new extension version 2.
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
